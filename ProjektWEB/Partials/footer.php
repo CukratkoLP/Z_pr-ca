629162ce09f8800ac3082d31e0dfa703f99a1444
Lukáš Hošek <161839046+CukratkoLP@users.noreply.github.com>
2024-05-18 14:31:53 +0200

    Add files via upload

---
+++ ProjektWEB/Partials/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+        <p class="text1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id accumsan sapien. Praesent varius, nisi nec hendrerit interdum, mauris tortor varius neque, eleifend posuere nunc elit vitae massa. Duis magna erat, commodo sit amet elementum pharetra, ultricies sed arcu. Aenean ante velit, ultrices gravida leo vel, semper faucibus nisl. Quisque elit nunc, condimentum nec orci a, malesuada pretium diam. Etiam tincidunt id diam eget laoreet. Fusce velit nisl, eleifend vitae commodo ac, fermentum porttitor purus. Donec sodales elit eu fermentum volutpat.</p>
+        <p class="text2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id accumsan sapien. Praesent varius, nisi nec hendrerit interdum, mauris tortor varius neque, eleifend posuere nunc elit vitae massa. Duis magna erat, commodo sit amet elementum pharetra, ultricies sed arcu. Aenean ante velit, ultrices gravida leo vel, semper faucibus nisl. Quisque elit nunc, condimentum nec orci a, malesuada pretium diam. Etiam tincidunt id diam eget laoreet. Fusce velit nisl, eleifend vitae commodo ac, fermentum porttitor purus. Donec sodales elit eu fermentum volutpat.</p>
+        <p class="text3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id accumsan sapien. Praesent varius, nisi nec hendrerit interdum, mauris tortor varius neque, eleifend posuere nunc elit vitae massa. Duis magna erat, commodo sit amet elementum pharetra, ultricies sed arcu. Aenean ante velit, ultrices gravida leo vel, semper faucibus nisl. Quisque elit nunc, condimentum nec orci a, malesuada pretium diam. Etiam tincidunt id diam eget laoreet. Fusce velit nisl, eleifend vitae commodo ac, fermentum porttitor purus. Donec sodales elit eu fermentum volutpat.</p>
+        <p class="kontakt"></p>
+    </footer>
+</body>
+<script src="JS/script.js"></script>
+</html>
---
+++ ProjektWEB/Partials/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Lukasko04</title>
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">Logo</a>     
+        <nav class="navbar">
+            <a href="domov.php">Domov</a>
+            <a href="Minihry.php">Minihry</a>
+            <a href="onas.php">Kto sme</a>
+            <a href="Q&A.php">Q&A</a>
+            <a href="login.php">Login</a>
+        </nav>
+        
+    </header>
